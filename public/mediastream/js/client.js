e86919ab1be7073c683e759ccfa7a7dc5c2f4154
jiangkan <18369564020@163.com>
2020-02-06 22:44:38 +0800

    删除了适配器，并添加了显示相关信息的代码

--- public/mediastream/js/client.js
+++ public/mediastream/js/client.js
@@ -1,8 +1,34 @@
 'use strict'
 
+
+var audioSource = document.querySelector('select#audioSource');
+var videoSource = document.querySelector('select#videoSource');
+var audioOutput = document.querySelector('select#audioOutput');
+
 var videoplay = document.querySelector('video#player');
 function gotMediaStream(stream) {
     videoplay.srcObject = stream;
+    return navigator.mediaDevices.enumerateDevices();
+}
+
+
+
+function gotDevices(deviceInfos) {
+    deviceInfos.forEach(function (deviceInfo) {
+
+        var option = document.createElement('option');
+        option.text = deviceInfo.label;
+        option.value = deviceInfo.deviceId;
+
+        if (deviceInfo.kind === 'audioinput') {
+            audioSource.appendChild(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+            videoSource.appendChild(option);
+        } else if (deviceInfo.kind === 'audiooutput') {
+            audioOutput.appendChild(option);
+        }
+
+    });
 }
 
 function handleError(err) {
@@ -19,5 +45,6 @@ if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
 
     navigator.mediaDevices.getUserMedia(constraints)
         .then(gotMediaStream)
+        .then(gotDevices)
         .catch(handleError)
 }
