946afd8d661bc69f747354aac03c4923afb9b722
jiangkan <18369564020@163.com>
2020-02-09 13:48:59 +0800

    显示设备信息

--- public/mediastream/js/client.js
+++ public/mediastream/js/client.js
@@ -1,13 +1,37 @@
 'use strict'
 
+
+var audioSource = document.querySelector('select#audioSource');
+var audioOutput = document.querySelector('select#audioOutput');
+var videoSource = document.querySelector('select#videoSource')
 var videoplay = document.querySelector('video#player');
+function gotDevices(deviceInfos) {
+    deviceInfos.forEach(function (deviceInfo) {
+
+        var option = document.createElement('option')
+        option.text = deviceInfo.label;
+        option.value = deviceInfo.deviceId;
+
+        if (deviceInfo.kind === 'audioiput') {
+            audioSource.appendChild(option);
+        }else if (deviceInfo.kind === 'audiooutput') {
+            audioOutput.appendChild(option);
+        }else if (deviceInfo.kid === 'videoinput') {
+            videoSource.appendChild(option);
+        }
+
+    });
+}
 
 function gotMediaStream(stream) {
     videoplay.srcObject = stream;
+    return navigator.mediaDevices.enumerateDevices();
 }
 function handleError(err) {
     console.log('getUserMedia error:', err);
 }
+
+
 
 if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
     console.log('getUserMedia is not supported!');
@@ -19,5 +43,6 @@ if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
     }
     navigator.mediaDevices.getUserMedia(constraints)
         .then(gotMediaStream)
+        .them(gotDevices)
         .catch(handleError);
 }
