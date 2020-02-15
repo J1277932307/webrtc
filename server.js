e540c2e0e10851e0aa5ba4a05b07eb62214d112c
jiangkan <18369564020@163.com>
2020-02-15 13:27:00 +0800

    调试bug

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ var options = {
 var https_server = https.createServer(options, app);
 var io = socketIo.listen(https_server);
 
+
 /*io.sockets.on('connection',(socket)=>{
 
     socket.on('message',(room,data)=>{
@@ -92,36 +93,54 @@ var io = socketIo.listen(https_server);
         //socket.broadcast.emit('joined',room,socket.id); //除自己，节点上全部
     });
 })*/
-sockio.sockets.on('connection', (socket)=>{
+io.sockets.on('connection', (socket)=>{
 
     socket.on('message', (room, data)=>{
-        socket.to(room).emit('message', room, socket.id, data)//房间内所有人
+        socket.to(room).emit('message', room, socket.id, data)//房间内所有人,除自己外
     });
 
+    //该函数应该加锁
     socket.on('join', (room)=> {
+
         socket.join(room);
-        var myRoom = sockio.sockets.adapter.rooms[room];
+
+        var myRoom = io.sockets.adapter.rooms[room];
         var users = Object.keys(myRoom.sockets).length;
+
         logger.log('the number of user in room is: ' + users);
-        socket.emit('joined', room, socket.id);
+
+        //在这里可以控制进入房间的人数,现在一个房间最多 2个人
+        //为了便于客户端控制，如果是多人的话，应该将目前房间里
+        //人的个数当做数据下发下去。
+        if(users < 3) {
+            socket.emit('joined', room, socket.id);
+            if (users > 1) {
+                socket.to(room).emit('otherjoin', room);//除自己之外
+            }
+        }else {
+            socket.leave(room);
+            socket.emit('full', room, socket.id);
+        }
         //socket.to(room).emit('joined', room, socket.id);//除自己之外
         //io.in(room).emit('joined', room, socket.id)//房间内所有人
         //socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点
     });
 
     socket.on('leave', (room)=> {
-        var myRoom = sockio.sockets.adapter.rooms[room];
+        var myRoom = io.sockets.adapter.rooms[room];
         var users = Object.keys(myRoom.sockets).length;
         //users - 1;
 
         logger.log('the number of user in room is: ' + (users-1));
 
         socket.leave(room);
+        socket.to(room).emit('bye', room, socket.id)//房间内所有人,除自己外
         socket.emit('leaved', room, socket.id);
         //socket.to(room).emit('joined', room, socket.id);//除自己之外
         //io.in(room).emit('joined', room, socket.id)//房间内所有人
         //socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点
     });
+
 });
 
 
