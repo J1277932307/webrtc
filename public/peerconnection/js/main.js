bd4c8dba28db7c6ee8cdfc0b8b03d77f2da67a75
jiangkan <18369564020@163.com>
2020-02-29 15:32:59 +0800

    视频通话

--- public/peerconnection/js/main.js
+++ public/peerconnection/js/main.js
@@ -8,6 +8,9 @@ var btnHangup = document.querySelector('button#hangup');
 
 var localStream;
 
+var pc1;
+var pc2;
+
 function getMediaStream(stream) {
     localVideo.srcObject = stream;
     localStream = stream;
@@ -32,7 +35,68 @@ function start(){
     }
 }
 
+function getRemoteStream(e) {
+    remoteVide.srcObject = e.streams[0];
 
+}
+
+function handlerOfferError(err) {
+    console.error('Failed to create offer:', err);
+}
+function getOffer(desc) {
+    pc1.setLocalDescription(desc);
+
+    pc2.setRemoteDescription(desc);
+
+    pc2.createAnswer()
+        .then(getAnswer)
+        .catch(handleAnswerError);
+
+}
+
+function handlerAnswerError(err) {
+    console.error('Failed to create Answer!', err);
+}
+function getAnswer(desc) {
+    pc2.setLocalDescription(desc);
+
+    pc1.setRemoteDescription(desc);
+}
+
+function call(){
+    pc1 = new RTCPeerConnection();
+    pc2 = new RTCPeerConnection();
+
+    pc1.onicecandidate = (e)=>{
+        pc2.addIceCandidate(e.candidate);
+    }
+    pc2.onicecandidate = (e)=>{
+        pc1.addIceCandidate(e.candidate);
+    }
+    pc2.ontrack = getRemoteStream;
+
+    localStream.getTracks().forEach((track)=>{
+        pc1.addTrack(track,localStream);
+    })
+
+    var offerOptions = {
+        offerToReceiveAudio:0,
+        offerToReceiveVideo: 1
+    }
+    pc1.createOffer(offerOptions)
+        .then(getOffer)
+        .catch(handleOfferError);
+
+
+
+}
+
+function hangup(){
+    pc1.close();
+    pc2.close();
+    pc1 = null;
+    pc2 = null;
+}
 btnStart.onclick = start;
 btnCall.onclick = call;
 btnHangup.onclick = hangup;
